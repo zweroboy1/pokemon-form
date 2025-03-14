405efa8f00974674ef6fd5dce7b5d40092d5d821
zweroboy1 <fprognoz@gmail.com>
2025-03-14 16:55:47 +0200

    doc: add story for select

---
+++ src/components/PokemonSelect.stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { useForm, UseFormRegister } from 'react-hook-form';
+import PokemonSelect from './PokemonSelect';
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  team?: string[];
+}
+
+const meta: Meta<typeof PokemonSelect> = {
+  title: 'Components/PokemonSelect',
+  component: PokemonSelect,
+};
+export default meta;
+
+const Template: StoryFn<typeof PokemonSelect> = (args) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <PokemonSelect
+        {...args}
+        register={register as UseFormRegister<FormData>} 
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  onPokemonDataChange: (pokemons) => console.log('Pokemon Data Changed:', pokemons),
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+  ...Default.args,
+  error: 'You must select exactly 4 Pokémon',
+};
